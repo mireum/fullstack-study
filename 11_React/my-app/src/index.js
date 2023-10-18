b40abf79ba01ad081b8000a0dd0f77b03024ea25
fkdkskdfjfk <yljw225@naver.com>
2023-10-18 16:35:53 +0900

    Clock function

--- 11_React/my-app/src/index.js
+++ 11_React/my-app/src/index.js
@@ -5,14 +5,27 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import JsxUse from './chapter3/3.4/JsxUse';
 import Library from './chapter3/Library';
+import Clock from './chapter4/Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// 4장 예제
+// 1초마다 Clock 컴포넌트를 렌더링하는 코드
+// (실제 개발에서 이렇게 쓰는 경우는 없고 Stage를 변경해서 재렌더링 시킴)
+setInterval(() => {
+  root.render(
+    <Clock />
+  );
+}, 1000);
+
 root.render(
   // <App />
 
   // 3장 예제
   // <JsxUse />
-  <Library />
+  // <Library />
+
+  <Clock />
 );
 
 // If you want to start measuring performance in your app, pass a function
