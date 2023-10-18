7cf52af7ff6158e5ca2a522fd19f92abcd78c2e1
fkdkskdfjfk <yljw225@naver.com>
2023-10-18 17:38:39 +0900

    change env

---
+++ 11_React/my-app/src/chapter5/5.3/Layout.jsx
@@ -0,0 +1,12 @@
+function Layout(props) {
+  console.log(props);
+
+  // Quiz: {} 채우기
+  return (
+    <>
+      레이아웃 크기는 가로 {}, 세로 {}
+    </>
+  );
+}
+
+export default Layout;
--- 11_React/my-app/src/chapter5/5.3/PropsUse.jsx
+++ 11_React/my-app/src/chapter5/5.3/PropsUse.jsx
@@ -1,3 +1,6 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Layout from "./Layout";
 import Profile from "./Profile";
 
 function PropsUse() {
@@ -12,6 +15,17 @@ function PropsUse() {
         viewCount={999} name="최지우" introduction="안녕하세요 헬로"
       />
 
+      <Layout 
+        width={2500}
+        height={1440}
+        // props로 다른 컴포넌트를 넘기는 것도 가능
+        header={
+          <Header title="지우의 블로그입니다." />
+        }
+        fppter={
+          <Footer />
+        }
+      />
     </>
   );
 }
