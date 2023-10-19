a8bf2e55f816f8a14e7439f9c0e18d8e0f985aaa
fkdkskdfjfk <yljw225@naver.com>
2023-10-19 16:43:04 +0900

    useState quiz

--- 11_React/my-app/src/chapter7/7.2/Counter.jsx
+++ 11_React/my-app/src/chapter7/7.2/Counter.jsx
@@ -7,17 +7,20 @@ function Counter() {
   console.log(useState(0));
   // useState()의 결과로 배열이 반환된다 => [초기값, set함수]
   // 배열의 구조 분해 할당을 사용하여 변수 선언 및 할당
-
+  
   // (참고) ES5
   // const arr = useState(0);
   // const count = arr[0];
   // const setCount = arr[1];
-
+  
   // 만약 state 미사용 시
   // 이런 식으로 쓰면 카운트 값을 증가시킬 수는 있지만, 재렌더링이 일어나지 않음
   // 다른 이유로 재렌더링 발생 시, 값이 초기화됨
   let wrongCount = 0;
   
+  const [color, setColor] = useState('blue');
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
     <>
       {/* 1. state 사용 */}
@@ -44,6 +47,19 @@ function Counter() {
         console.log(wrongCount);
       }}>클릭(잘못된 방법)</button>
 
+
+      {/* Quiz: 글자색 바꾸기, 버튼 내용 바꾸기 */}
+      <hr />
+
+      <p style={{ color: color }}>현재 글자색: {color}</p>
+      <button onClick={() => {setColor('red')}}>글자색 변경</button>
+
+
+      <p>버튼 내용 바꾸기(힌트: 삼항 연산자 사용)</p>
+      <button onClick={() => { setIsClicked(true); }}>
+        {isClicked ? '구독 완료' : '구독'}
+      </button>
+      
     </>
   );
 }
