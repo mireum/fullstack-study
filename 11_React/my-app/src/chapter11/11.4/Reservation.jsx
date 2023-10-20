7d8f1b6e053b226ffb3ed8bf361f5b20cb8c279b
fkdkskdfjfk <yljw225@naver.com>
2023-10-20 20:47:21 +0900

    radio type

--- 11_React/my-app/src/chapter11/11.4/Reservation.jsx
+++ 11_React/my-app/src/chapter11/11.4/Reservation.jsx
@@ -4,20 +4,28 @@ function Reservation() {
   // 여러 개의 입력 제어하기 => 여러 개의 state 선언
   const [breakfast, setBreakfast] = useState(false);
   const [numberOfGuests, setNumberOfGuests] = useState(2);
+  const [roomType, setRoomType] = useState('SINGLE');
 
   const handleBreakfastChange = (e) => {
+    console.log(e.target.checked);
     setBreakfast(e.target.checked);
   };
 
   const handleGuestsChange = (e) => {
+    console.log(e.target.value);
     setNumberOfGuests(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert(`조식 여부: ${breakfast}, 투숙객 수: ${numberOfGuests}`);
+    alert(`조식 여부: ${breakfast}, 투숙객 수: ${numberOfGuests}, 룸 타입: ${roomType}`);
   };
-  
+
+  const handleRoomChange = (e) => {
+    console.log(e.target.value);
+    setRoomType(e.target.value);
+  };
+
   return (
     <form onSubmit={handleSubmit}>
       <label>
@@ -39,6 +47,41 @@ function Reservation() {
           onChange={handleGuestsChange}  
         />
       </label>
+
+      <br />
+
+      룸 타입:
+      <label>
+        <input 
+          type="radio" 
+          name="roomType"
+          value="SINGLE"
+          checked={roomType==="SINGLE"}
+          onChange={handleRoomChange}
+        />  
+        싱글
+      </label>
+      <label>
+        <input 
+          type="radio"
+          name="roomType" 
+          value="DOUBLE"
+          checked={roomType==="DOUBLE"}
+          onChange={handleRoomChange}
+        />
+        더블
+      </label>
+      <label>
+        <input 
+          type="radio" 
+          name="roomType"
+          value="TWIN"
+          checked={roomType==="TWIN"}
+          onChange={handleRoomChange}
+        />
+        트윈
+      </label>
+
 
       <button type="submit">제출</button>
     </form>
