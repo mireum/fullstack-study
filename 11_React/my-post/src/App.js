156d035fa2e1a8b6ea49203ce4f138a5ed5ccbf9
fkdkskdfjfk <yljw225@naver.com>
2023-10-23 20:25:39 +0900

    unshift

--- 11_React/my-post/src/App.js
+++ 11_React/my-post/src/App.js
@@ -16,6 +16,9 @@ function App() {
 
   const [showPostDetail, setShowPostDetail] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [likeCount, setLikeCount] = useState([0, 0, 0]);
+  const [value, setValue] = useState('');
+
 
   return (
     <>
@@ -59,6 +62,44 @@ function App() {
               <h4>{el}</h4>
               <p>2023년 1월 20일</p>
               <p>by kim</p>
+
+              <hr />
+
+              <div className='toolbar'>
+                {/* 좋아요 기능 */}
+                <span onClick={(e) => {
+                  // (버그 수정)
+                  // 현재는 좋아요 버튼을 누를 때 글 상세보기까지 같이 클릭됨!!
+                  // 부모 - 자식 관계에 있을 때 이벤트 버블링이 일어남
+                  e.stopPropagation();  // 상위 요소로 퍼지는 이벤트 버블링을 막음
+
+                  const copyLikeCount = [...likeCount]; // 배열의 복사본 만들기(새로운 배열)
+                  copyLikeCount[index]++;
+                  setLikeCount(copyLikeCount);
+                }}>
+                  🤎 {likeCount[index]}
+                </span>
+
+                {/* 포스트 삭제하기 */}
+                <span style={{ fontSize: 27 }} onClick={(e) => {
+                  e.stopPropagation();
+
+                  // Quiz: 삭제 기능 만들기
+                  // div 하나를 직접 제거 하는 것 X
+                  // state에서 제거하면 알아서 자동으로 렌더링 O
+                  // const copyPosts = [...posts];
+                  // copyPosts.splice(index, 1);
+                  // setPosts(copyPosts);
+
+                  // 또는 배열의 filter() 함수 사용
+                  const filteredPosts = posts.filter((value, idx) => {
+                    return index !== idx;  
+                  });
+                  setPosts(filteredPosts);
+                }}>
+                  🗑
+                </span>
+              </div>
             </div>
           );
         })}
@@ -66,8 +107,28 @@ function App() {
 
 
       {/* 포스트 상세보기 */}
+      {/* Quiz: input에 제목 입력 후 등록 버튼 누르면 맨 앞에 새로운 포스트 추가
+      
+      1) input을 제어 컴포넌트로 만들어서 사용자가 입력한 값을 state로 저장해서 관리
+      2) 등록 버튼 클릭 시 posts 상태에 맨 앞에 새로운 데이터 추가
+      */}
+      <input type='text' onChange={(e) => {
+        console.log(e.target);
+        setValue(e.target.value);
+      }}/>
+      <button type='button' onClick={(e) => {
+        // div 하나를 새로 생성 X
+        // posts state에 요소 하나 추가하면 자동으로 렌더링 O
+        const copiedPost = [...posts];
+        copiedPost.unshift(value);
+        setPosts(copiedPost);
+      }}>
+        포스트 등록
+      </button>
+
+
       {/* Quiz: 조건부 렌더링 */}
-      {showPostDetail && <PostDetail posts={posts} currentIndex={currentIndex}/>}
+      {showPostDetail && <PostDetail posts={posts} currentIndex={currentIndex} setPosts={setPosts} />}
 
 
     </>
--- 11_React/my-post/src/components/PostDetail.jsx
+++ 11_React/my-post/src/components/PostDetail.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 
 function PostDetail(props) {
+  const { posts, currentIndex, setPosts } = props;
   return (
-    <div>
-      <h4>제목: {props.posts[props.currentIndex]}</h4>
+    <div className='detail'>
+      <h4>제목: {posts[currentIndex]}</h4>
       <p>날짜: 2023년 1월 20일</p>
       <p>작성자: goni.kim</p>
       <p>... 상세 내용 ...</p>
+
+      {/* 간단한 포스트 수정하기 */}
+      <button onClick={() => {
+        // 배열이나 객체의 state를 변경하는 법
+        // 새로운 배열 또는 객체를 만들어 변경하고 set함수에 넣어줘야 함
+        const copyPosts = [...posts];   // 배열의 복사본 만들기(새로운 배열)
+        copyPosts[currentIndex] = `${copyPosts[currentIndex]} - 수정`;
+        setPosts(copyPosts);
+      }}>
+        수정하기
+      </button>
     </div>
   );
 }
