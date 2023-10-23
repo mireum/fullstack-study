680c7909a08703627c5cf3523a9322480049c875
fkdkskdfjfk <yljw225@naver.com>
2023-10-23 21:12:49 +0900

    1. PostListItem 컴포넌트 추출

--- 11_React/my-post/src/App.js
+++ 11_React/my-post/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import PostDetail from './components/PostDetail';
+import PostListItem from './components/PostListItem';
 
 // POST앱 CRUD 만들기
 // C: Create(등록)
@@ -34,80 +35,11 @@ function App() {
       </header>
 
       <div className='inner'>
-        {/* 포스트 목록 */}
-        {/* <div className='list'>
-          <h4>{posts[0]}</h4>
-          <p>2023년 1월 20일</p>
-          <p>by goni.kim</p>
-        </div>
-        <div className='list'>
-          <h4>{posts[1]}</h4>
-          <p>2023년 1월 2일</p>
-          <p>by alice</p>
-        </div>
-        <div className='list'>
-          <h4>{posts[2]}</h4>
-          <p>2023년 12월 20일</p>
-          <p>by hero</p>
-        </div> */}
-
-        {/* Quiz: map()을 이용하여 posts 배열 반복 출력하기 */}
-        
-        {posts.map((el, index) => {
-          return (
-            <div key={index} className='list' onClick={() => {
-              setShowPostDetail(true);
-              setCurrentIndex(index);
-            }}>
-              <h4>{el}</h4>
-              <p>2023년 1월 20일</p>
-              <p>by kim</p>
-
-              <hr />
-
-              <div className='toolbar'>
-                {/* 좋아요 기능 */}
-                <span onClick={(e) => {
-                  // (버그 수정)
-                  // 현재는 좋아요 버튼을 누를 때 글 상세보기까지 같이 클릭됨!!
-                  // 부모 - 자식 관계에 있을 때 이벤트 버블링이 일어남
-                  e.stopPropagation();  // 상위 요소로 퍼지는 이벤트 버블링을 막음
-
-                  const copyLikeCount = [...likeCount]; // 배열의 복사본 만들기(새로운 배열)
-                  copyLikeCount[index]++;
-                  setLikeCount(copyLikeCount);
-                }}>
-                  🤎 {likeCount[index]}
-                </span>
-
-                {/* 포스트 삭제하기 */}
-                <span style={{ fontSize: 27 }} onClick={(e) => {
-                  e.stopPropagation();
-
-                  // Quiz: 삭제 기능 만들기
-                  // div 하나를 직접 제거 하는 것 X
-                  // state에서 제거하면 알아서 자동으로 렌더링 O
-                  // const copyPosts = [...posts];
-                  // copyPosts.splice(index, 1);
-                  // setPosts(copyPosts);
-
-                  // 또는 배열의 filter() 함수 사용
-                  const filteredPosts = posts.filter((value, idx) => {
-                    return index !== idx;  
-                  });
-                  setPosts(filteredPosts);
-
-                  // (버그 수정) 삭제 시 해당 포스트의 좋아요 카운트도 같이 삭제
-                  const copyLikeCount = [...likeCount];
-                  copyLikeCount.splice(index, 1);
-                  setLikeCount(copyLikeCount);
-                }}>
-                  🗑
-                </span>
-              </div>
-            </div>
-          );
-        })}
+        <PostListItem posts={posts} setPosts={setPosts}
+          setShowPostDetail={setShowPostDetail}
+          currentIndex={currentIndex} setCurrentIndex={setCurrentIndex}
+          likeCount={likeCount} setLikeCount={setLikeCount}
+        />
       </div>
 
 
@@ -160,3 +92,23 @@ export default App;
 // 새로고침 시 HTML/CSS/JS 파일을 다시 읽어오기 때문
 // 데이터를 유지하려면 서버에 보내서 DB에 영구 저장하고
 // 새로고침 발생 시 DB에서 다시 읽어오면 됨
+
+
+
+// <추가 개선 과제>
+// 1. PostListItem 컴포넌트 추출 
+
+// 2. 날짜 및 작성자, 좋아요 수 등 데이터를 문자열이 아닌 객체 형태로 처리해보기
+// state에 글 제목만 저장되어 있는게 아니라 날짜같은 것도 저장해두고 보여주는 식이면 굿
+// => 글 등록 시 현재 날짜까지 같이 저장되도록 하면 나이스
+
+// 3. input에 아무것도 입력안하고 등록 버튼 누르는거 막기
+// 유저의 의도치않은 행동을 막는 코드도 많이 짜야 안전한 사이트가 됨
+// 1) 미입력시 비활성화 -> 입력이 생기면 버튼 활성화
+// 2) 등록 버튼 누를 시 유효성 검사
+
+// 4. 포스트 수정할 때 input으로 입력받은 내용으로 수정해보기
+
+// 5. 글 오름차순 정렬
+
+// 6. 그 외 개선 및 구현할 것 많으니 자유롭게 연습해보기
---
+++ 11_React/my-post/src/components/PostListItem.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+
+function PostListItem(props) {
+  const { posts, setPosts,  setShowPostDetail,
+    currentIndex, setCurrentIndex, likeCount, setLikeCount} = props;
+  
+
+  return (
+    <div>
+      {/* 포스트 목록 */}
+        {/* <div className='list'>
+          <h4>{posts[0]}</h4>
+          <p>2023년 1월 20일</p>
+          <p>by goni.kim</p>
+        </div>
+        <div className='list'>
+          <h4>{posts[1]}</h4>
+          <p>2023년 1월 2일</p>
+          <p>by alice</p>
+        </div>
+        <div className='list'>
+          <h4>{posts[2]}</h4>
+          <p>2023년 12월 20일</p>
+          <p>by hero</p>
+        </div> */}
+
+        {/* Quiz: map()을 이용하여 posts 배열 반복 출력하기 */}
+        {posts.map((el, index) => {
+          return (
+            <div key={index} className='list' onClick={() => {
+              setShowPostDetail(true);
+              setCurrentIndex(index);
+            }}>
+              <h4>{el}</h4>
+              <p>2023년 1월 20일</p>
+              <p>by kim</p>
+
+              <hr />
+
+              <div className='toolbar'>
+                {/* 좋아요 기능 */}
+                <span onClick={(e) => {
+                  // (버그 수정)
+                  // 현재는 좋아요 버튼을 누를 때 글 상세보기까지 같이 클릭됨!!
+                  // 부모 - 자식 관계에 있을 때 이벤트 버블링이 일어남
+                  e.stopPropagation();  // 상위 요소로 퍼지는 이벤트 버블링을 막음
+
+                  const copyLikeCount = [...likeCount]; // 배열의 복사본 만들기(새로운 배열)
+                  copyLikeCount[index]++;
+                  setLikeCount(copyLikeCount);
+                }}>
+                  🤎 {likeCount[index]}
+                </span>
+
+                {/* 포스트 삭제하기 */}
+                <span style={{ fontSize: 27 }} onClick={(e) => {
+                  e.stopPropagation();
+
+                  // Quiz: 삭제 기능 만들기
+                  // div 하나를 직접 제거 하는 것 X
+                  // state에서 제거하면 알아서 자동으로 렌더링 O
+                  // const copyPosts = [...posts];
+                  // copyPosts.splice(index, 1);
+                  // setPosts(copyPosts);
+
+                  // 또는 배열의 filter() 함수 사용
+                  const filteredPosts = posts.filter((value, idx) => {
+                    return index !== idx;  
+                  });
+                  setPosts(filteredPosts);
+
+                  // (버그 수정) 삭제 시 해당 포스트의 좋아요 카운트도 같이 삭제
+                  const copyLikeCount = [...likeCount];
+                  copyLikeCount.splice(index, 1);
+                  setLikeCount(copyLikeCount);
+                }}>
+                  🗑
+                </span>
+              </div>
+            </div>
+          );
+        })}
+    </div>
+  );
+}
+
+export default PostListItem;
