e23f42e1f3b35987ba0888579a3c098036a5dd91
fkdkskdfjfk <yljw225@naver.com>
2023-10-23 17:16:27 +0900

    Quiz: map()으로 posts배열 반복 출력

--- 11_React/my-post/src/App.js
+++ 11_React/my-post/src/App.js
@@ -1,24 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+// POST앱 CRUD 만들기
+// C: Create(등록)
+// R: Read(목록, 상세보기)
+// U: Update(수정)
+// D: Delete(삭제)
 
 function App() {
+  // 서버에서 가져온 데이터라고 가정
+  const [posts, setPosts] = useState(['리액트 잘 쓰려면?', 
+  '자바스크립트 핵심 문법', '스타일링 가이드']);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      {/* 상단 헤더 만들기 */}
+      <header className='header--dark'>
+        <h4>Gonilog</h4>
+        <nav>
+          <ul>
+            <li>트렌딩</li>
+            <li>최신</li>
+          </ul>
+        </nav>
       </header>
-    </div>
+
+      <div className='inner'>
+        {/* 포스트 목록 */}
+        <div className='list'>
+          <h4>{posts[0]}</h4>
+          <p>2023년 1월 20일</p>
+          <p>by goni.kim</p>
+        </div>
+        <div className='list'>
+          <h4>{posts[1]}</h4>
+          <p>2023년 1월 2일</p>
+          <p>by alice</p>
+        </div>
+        <div className='list'>
+          <h4>{posts[2]}</h4>
+          <p>2023년 12월 20일</p>
+          <p>by hero</p>
+        </div>
+
+        {/* Quiz: map()을 이용하여 posts 배열 반복 출력하기 */}
+        
+        {posts.map(el => {
+          return (
+            <div className='list'>
+              <h4>{el}</h4>
+              <p>2023년 1월 20일</p>
+              <p>by kim</p>
+            </div>
+          )
+        })}
+          
+        
+      </div>
+    </>
   );
 }
 
