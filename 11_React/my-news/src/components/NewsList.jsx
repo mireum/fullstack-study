1ebb0d9a94b88815b97068f81212e64369c7be1f
fkdkskdfjfk <yljw225@naver.com>
2023-10-31 20:39:26 +0900

    newsList

---
+++ 11_React/my-news/src/components/NewsList.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from "styled-components";
+import NewsItem from './NewsItem';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+// 샘플 데이터 렌더링 해보기
+const sampleArticle = {
+  title: '제목',
+  description: '내용',
+  url: 'https://google.com',
+  urlToImage: 'https://via.placeholder.com/160'
+};
+
+
+// API를 요청하고 뉴스 데이터가 들어있는 배열을 리액트 컴포넌트 배열로 변환하여 렌더링하는 컴포넌트
+function NewsList(props) {
+  return (
+    <NewsListBlock>
+      <NewsItem article={sampleArticle} />
+      <NewsItem article={sampleArticle} />
+      <NewsItem article={sampleArticle} />
+      <NewsItem article={sampleArticle} />
+      <NewsItem article={sampleArticle} />
+      <NewsItem article={sampleArticle} />
+    </NewsListBlock>
+  );
+}
+
+export default NewsList;
