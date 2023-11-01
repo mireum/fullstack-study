930032a508211b3b8c539c0ec21de689a2494fa7
fkdkskdfjfk <yljw225@naver.com>
2023-11-01 17:58:42 +0900

    productList redux

--- 11_React/my-app/src/chapter18/features/ProductList.jsx
+++ 11_React/my-app/src/chapter18/features/ProductList.jsx
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addToProductList } from './productSlice';
+import { addToProductList, selectProductList } from './productSlice';
 
 
 function ProductList(props) {
-  const product = useSelector(state => state.product.productList);
+  // const productList = useSelector(state => state.product.productList);
+  const productList = useSelector(selectProductList);
 
   const dispatch = useDispatch();
 
   const [productName, setProductName] = useState('');
 
+  const handleAddProduct = () => {
+    dispatch(addToProductList(productName));
+    setProductName('');
+  };
 
   return (
     <>
@@ -19,22 +24,22 @@ function ProductList(props) {
           type="text" 
           value={productName}
           onChange={(e) => { setProductName(e.target.value) }}
+          onKeyUp={(e) => {
+            if (e.key === 'Enter') {
+              handleAddProduct();
+            }
+          }}
         />
         <button
           type='button'
-          onClick={() => {
-            return (
-            dispatch(addToProductList(productName)),
-            setProductName('')
-            );
-          }}
+          onClick={() => {handleAddProduct()}}
         >
           추가
         </button>
       </p>
       <p>상품 목록</p>
       <ul>
-        {product && product.map((el) => <li>{el}</li>)}
+        {productList && productList.map((el, index) => <li key={index}>{el}</li>)}
         
       </ul>
     </>
--- 11_React/my-app/src/chapter18/features/productSlice.js
+++ 11_React/my-app/src/chapter18/features/productSlice.js
@@ -8,12 +8,15 @@ export const productSlice = createSlice({
   name: 'product',
   initialState,
   reducers: {
-    addToProductList: (state, action) => {
-      state.productList.push(action.payload);
+    addToProductList: (state, { payload: productName }) => {
+      state.productList.push(productName);
     }
   }
 });
 
 export const { addToProductList } = productSlice.actions;
+
+// 데이터를 가공해서 export 할 수도 있음
+export const selectProductList = state => state.product.productList;
 
 export default productSlice.reducer;
