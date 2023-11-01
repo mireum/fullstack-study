409481e6e433b653e42f7e978ca35226998408ba
fkdkskdfjfk <yljw225@naver.com>
2023-11-01 18:58:13 +0900

    redux 마무리

--- 11_React/my-app/src/chapter18/features/productSlice.js
+++ 11_React/my-app/src/chapter18/features/productSlice.js
@@ -19,4 +19,6 @@ export const { addToProductList } = productSlice.actions;
 // 데이터를 가공해서 export 할 수도 있음
 export const selectProductList = state => state.product.productList;
 
+// default는 한 파일 당 한번만 쓸 수 있다. 
+// default로 export할 것은 import로 가져올 때 괄호를 치면 안된다
 export default productSlice.reducer;
