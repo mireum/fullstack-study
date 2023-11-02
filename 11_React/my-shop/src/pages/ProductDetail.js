68e718f0f7d096370f2d57f8ad2c27a37ad471dd
fkdkskdfjfk <yljw225@naver.com>
2023-11-02 16:26:21 +0900

    alert창에 애니메이션 적용

--- 11_React/my-shop/src/pages/ProductDetail.js
+++ 11_React/my-shop/src/pages/ProductDetail.js
@@ -4,6 +4,17 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getSelectedProduct, selectSelectedProduct } from '../features/product/productSlice';
 import axios from 'axios';
+import styled, { keyframes } from 'styled-components';
+
+// 스타일드 컴포넌트를 이용한 애니메이션 속성 적용
+const highlight = keyframes`
+  from { background-color: #cff4fc; }
+  50% { background-color: #e8f7fa; }
+  to{ background-color: #cff4fc; }
+`;
+const StyledAlert = styled(Alert)`
+  animation: ${highlight} 1s linear infinite; // from에서 to까지 1초동안 선형으로 무한히 반복
+`;
 
 function ProductDetail(props) {
   // URL 파라미터 가져오기
@@ -11,7 +22,7 @@ function ProductDetail(props) {
   const dispatch = useDispatch();
   const product = useSelector(selectSelectedProduct);
   
-  const [bool, setBool] = useState(true);
+  const [showInfo, setShowInfo] = useState(true); // Info Alert창 상태
 
   // 상품 상세보기가 처음 마운트 됐을 때 서버에 상품 id를 이용하여 데이터를 오청하고
   // 그 결과를 리덕스 스토어에 저장
@@ -30,37 +41,41 @@ function ProductDetail(props) {
   }, []);
   
   useEffect(() => {
-    setTimeout(() => {
-      setBool(false);
+    const timeout = setTimeout(() => {
+      setShowInfo(false);
     }, 3000);
+
+    // 불필요하게 타이머가 계속 쌓이는 것을 정리
+    return () => {
+      clearTimeout(timeout);
+    };
   }, []);
 
-  // const { id, title, content, price, imagePath } = product;
-
-
-  // 없는 상품일 때 예외 처리 -> 아래에 써야 함
+  
+  // 없는 상품일 때 예외 처리
   if (!product) {
     return null; // 아무것도 렌더링하지 않음
   } 
   
+  const { id, title, content, price, imagePath } = product; // if 로 product 확인하고 아래에 써야 함
 
   return (
     <Container className='pt-3'>
       {/* Quiz: Alert을 띄우고 3초 뒤에 사라지게 만들기
         힌트: 처음 렌더링 됐을 때 setTimeout으로 타이머 설정 */}
-      {bool && <Alert variant={'info'}>
+      {showInfo && <StyledAlert variant={'info'} onClose={() => setShowInfo(false)} dismissible >
         현재 34명이 이 상품을 보고 있습니다.
-      </Alert>}
+      </StyledAlert>}
 
       <Row>
         {/* Quiz: 데이터 바인딩 작업 */}
         <Col md={6}>
-          <img src={product.imagePath} width="80%" />
+          <img src={imagePath} width="80%" />
         </Col>
         <Col md={6}>
-          <h4 className='pt-5'>{product.title}</h4>
-          <p>{product.content}</p>
-          <p>₩{product.price}</p>
+          <h4 className='pt-5'>{title}</h4>
+          <p>{content}</p>
+          <p>₩{price}</p>
           <Button variant='primary'>주문하기</Button>
         </Col>
       </Row>
