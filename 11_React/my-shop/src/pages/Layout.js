d6ae31052b098890c1793660aa0a9a087041c543
fkdkskdfjfk <yljw225@naver.com>
2023-11-02 15:56:03 +0900

    Alert을 띄우고 3초 뒤에 사라지게 만들기

--- 11_React/my-shop/src/pages/Layout.js
+++ 11_React/my-shop/src/pages/Layout.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 function Layout(props) {
+  const navigate = useNavigate();
+
   return (
     <>
       {/* 헤더 영역: 상단 네비게이션 바 */}
       <header>
         <Navbar bg="dark" data-bs-theme="dark">
           <Container>
-            <Navbar.Brand href="#">뽀삐빠샵</Navbar.Brand>
+            <Navbar.Brand href="#" onClick={() => navigate('/')}>뽀삐빠샵</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link>홈</Nav.Link>
-              <Nav.Link>장바구니</Nav.Link>
+              <Nav.Link onClick={() => navigate('/')} >홈</Nav.Link>
+              <Nav.Link onClick={() => navigate('/cart')} >장바구니</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
--- 11_React/my-shop/src/pages/ProductDetail.js
+++ 11_React/my-shop/src/pages/ProductDetail.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Alert, Button, Col, Container, Row } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getSelectedProduct, selectSelectedProduct } from '../features/product/productSlice';
@@ -10,6 +10,8 @@ function ProductDetail(props) {
   const { productId } = useParams(); // App.js에서 지은 이름
   const dispatch = useDispatch();
   const product = useSelector(selectSelectedProduct);
+  
+  const [bool, setBool] = useState(true);
 
   // 상품 상세보기가 처음 마운트 됐을 때 서버에 상품 id를 이용하여 데이터를 오청하고
   // 그 결과를 리덕스 스토어에 저장
@@ -24,18 +26,41 @@ function ProductDetail(props) {
       }
     };
     fetchProductById();
+    
   }, []);
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setBool(false);
+    }, 3000);
+  }, []);
+
+  // const { id, title, content, price, imagePath } = product;
+
+
+  // 없는 상품일 때 예외 처리 -> 아래에 써야 함
+  if (!product) {
+    return null; // 아무것도 렌더링하지 않음
+  } 
+  
 
   return (
     <Container className='pt-3'>
+      {/* Quiz: Alert을 띄우고 3초 뒤에 사라지게 만들기
+        힌트: 처음 렌더링 됐을 때 setTimeout으로 타이머 설정 */}
+      {bool && <Alert variant={'info'}>
+        현재 34명이 이 상품을 보고 있습니다.
+      </Alert>}
+
       <Row>
+        {/* Quiz: 데이터 바인딩 작업 */}
         <Col md={6}>
-          <img src="https://www.yonexmall.com/shop/data/goods/1645767865278s0.png" width="80%" />
+          <img src={product.imagePath} width="80%" />
         </Col>
         <Col md={6}>
-          <h4 className='pt-5'>상품명</h4>
-          <p>상품설명</p>
-          <p>1000원</p>
+          <h4 className='pt-5'>{product.title}</h4>
+          <p>{product.content}</p>
+          <p>₩{product.price}</p>
           <Button variant='primary'>주문하기</Button>
         </Col>
       </Row>
