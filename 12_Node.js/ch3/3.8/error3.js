0bf9db2b98ad258aed1af3bba8c22debe509f267
fkdkskdfjfk <yljw225@naver.com>
2023-11-21 17:24:28 +0900

    자바스크립트에서 예외는 에러

---
+++ 12_Node.js/ch3/3.8/error3.js
@@ -0,0 +1,9 @@
+const fs = require('fs').promises;
+
+setInterval(() => {
+  // catch() 생략 시 문제됨, 습관적으로 쓰자!!
+  fs.unlink('./absdfdsf.js')
+    .catch(err => {
+      console.error(err);
+    });
+}, 1000);
