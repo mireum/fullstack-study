960253f2bffd8204130784f79d0a8460428f2cd4
fkdkskdfjfk <yljw225@naver.com>
2023-11-27 15:04:16 +0900

    req.body

--- 12_Node.js/ch6/6.2/learn-express/app.js
+++ 12_Node.js/ch6/6.2/learn-express/app.js
@@ -30,15 +30,44 @@ app.use(morgan('dev')); // 개발 시 사용
 // 위와 같은 함수가 morgan('')의 결과임
 
 
+// Express가 제공하는 static 미들웨어: static 파일들을 제공하는 미들웨어
+// static: 동적인 변동 사항이 없는 정적 파일들(예: css, js, image 등)
+// 폴더를 서버에 등록해두면 폴더 안의 정적 파일들을 사용 가능
+// app.use('요청 경로', express.static('실제 경로'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+// 예: localhost:3000/hello.css 로 요청이 들어오면 -> 실제 위치는 ~/learn-express/public/hello.css
+
+
+// (참고) 미들웨어끼리 순서 중요(성능적인 문제)
+// => 단순히 정적 리소스만 제공하면 되는데, 뒤에 위치하면 불필요한 미들웨어들이 실행됨
+// 예: 요청에 따라 static 미들웨어의 실행이 달라짐
+// localhost:3000/about.png 요청 -> /public 폴더 안에 해당 파일이 있으면 제공하고 여기서 끝
+// localhost:3000/about 요청 -> next()가 호출되어 다음 미들웨어 또는 /about 라우터까지 내려감
+
+
 // cookie-parser: 쿠키 관련 조작들이 편해짐
 // app.use(cookieParser([비밀키])); // 대괄호는 optional의 의미.
+// 서명(암호화)된 쿠키
+// 브라우저에 저장된 쿠키를 해커들이 못 쓰게끔 서명을 붙임(근데 모든 쿠키를 서명하지는 않음)
 app.use(cookieParser(process.env.COOKIE_SECRET)); // 넣어준다고 암호화가 되는건 아님. 옵션 넣어야함
+
+
+// body-parser: 옛날에는 body-parser를 많이 썼지만 요새는 express에 내장되서 안 씀
+// 요청 본문에 담겨오는 데이터가 알아서 파싱됨
+// 아래 2줄은 거의 필수로 들어가는 설정!!
+app.use(express.json());  // 클라이언트에서 json 데이터를 보냈을 때 파싱해서 req.body에 넣어줌
+app.use(express.urlencoded({ extended: true }));
+// 클라이언트에서 FormData를 보냈을 때(form을 submit할 때) 파싱해서 req.body에 넣어줌
+// extended: 퀴리스트링을 어떻게 처리할지(true면 qs 패키지를, false면 querystring 내장 모듈을 사용)
+// true를 추천, qs > querystring 보다 편의 및 강력한 기능 지원
+// 다만 FormData로 파일을 보내는 경우 urlencoded()로 처리 못함 => 이 떄는 multer 사용
 
 
 app.get('/', (req, res) => {
   // 쿠키 사용하기
   // 이전 방식: 임의로 만든 parseCookies() 함수를 사용해서 객체로 변환
   console.log(req.cookies); // 쿠키 문자열이 { mycookies: 'test' } 객체 형태로 알아서 파싱이 되어있음
+  console.log(req.signedCookies); // 서명(암호화)된 쿠키
 
   // 쿠키 설정하기
   // 이전 방식: 'Set-cookie': `name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`
@@ -48,10 +77,10 @@ app.get('/', (req, res) => {
     expires: new Date(Date.now() + 5 * 60 * 1000),  // 현재 시간에서 5분 뒤
     httpOnly: true, // JS에서 쿠키 접근 못하게 
     path: '/',  // 쿠키를 사용할 경로. '/'면 어디서든
-    signed: true, // 서명(암호화) 옵션: 쿠키 뒤에 서명이 붙음
+    signed: true, // 서명(암호화) 옵션: 쿠키 뒤에 서명이 붙음, 이 옵션 붙으면 log(req.cookies)에 안 찍히는거 확인
     // secure: true, // HTTPS일 경우에만 쿠키가 전송됨
   });
-  console.log(req.cookies); 
+
   
   // 쿠키 지우기
   // res.clearCookie(키, [옵션]); 옵션이 일치해야 지워짐
@@ -63,10 +92,17 @@ app.get('/', (req, res) => {
   // });
 
 
+  // req.body: 요청 본문에 담겨오는 데이터를 바로 쓸 수 있음
+  // console.log(req.body.name); // 나중에 직접 테스트 해볼 것! POST 요청 보내서 클라이언트 단에서 보내기
+
 
   res.sendFile(path.join(__dirname, '/index.html')); 
 });
 
+app.post((req, res) => {
+  // res.send('about 페이지입니다.');
+  console.log(req.body.name);
+});
 
 app.listen(app.get('port'), () => {
   console.log(app.get('port') + '번에서 익스프레스 서버 실행');
