cdec17a0fb3e055c4becf214be767194e8857946
fkdkskdfjfk <yljw225@naver.com>
2023-11-28 17:50:15 +0900

    라우터 그룹화하기

--- 12_Node.js/ch6/6.3/learn-express/route/user.js
+++ 12_Node.js/ch6/6.3/learn-express/route/user.js
@@ -44,7 +44,7 @@ router.post('/abc', (req, res) => {
 
 // router.route로 묶음
 // 주소를 먼저 써서 그룹화를 하고 그 다음에 메서드로 분기
-router.route('/abc')
+router.route('/abc')  // / 꼭 있어야 함
   .get((req, res) => {
     res.send('GET /abc');
   })
