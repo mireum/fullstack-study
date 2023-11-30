7e2004c4f31c3306ec1d467c2ec7d80b0d86fd3e
fkdkskdfjfk <yljw225@naver.com>
2023-11-30 14:54:29 +0900

    기본 설정

--- 12_Node.js/vilboard/app.js
+++ 12_Node.js/vilboard/app.js
@@ -18,34 +18,28 @@ dotenv.config();
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');  // view engine의 확장자 지정
 
 app.use(morgan('dev'));
-
-app.use('/', express.static(path.join(__dirname, 'public')));
-
+app.use('/', express.static(path.join(__dirname, 'public'))); // '/' 생략 가능
 app.use(cookieParser(process.env.COOKIE_SECRET));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(session({
   resave: false,
   saveUninitialized: false,
   secret: process.env.COOKIE_SECRET,
   cookie: {
-    httpOnly: true
+    httpOnly: true,
+    secure: false,  // 개발단계에선 false. 기본이 false
   },
   name: 'session-cookie',
 }));
 
-// '/' 라우터
-app.get('/', (req, res) => {
-
-  res.send('hello');
-});
-
 app.use((req, res, next) => {
-  res.status(404).send('404 NOT FOUND');
+  const error = new Error( `${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
 });
 
 app.use((err, req, res, next) => {
@@ -56,5 +50,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(app.get('port'), () => {
-  console.log('서버 실행!');
+  console.log(app.get('port'), '번 서버에서 대기 중');
 });
