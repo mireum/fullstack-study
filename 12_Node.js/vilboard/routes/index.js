21cfd32217da9fa1595357c53861ef498320a9d1
fkdkskdfjfk <yljw225@naver.com>
2023-11-30 17:09:19 +0900

    ejs 문법 중 include() 쓰면 다른 ejs 파일에 있는 HTML을 넣을 수 있다

--- 12_Node.js/vilboard/routes/index.js
+++ 12_Node.js/vilboard/routes/index.js
@@ -12,9 +12,17 @@ router.get('/', (req, res) => {
 
 // GET /insert 라우터
 // DB에 데이터 저장하기 테스트(테스트 후 데이터 삭제)
-router.get('/insert', (req, res) => {
-  db.collection('post').insertOne({ title: '제발 들어가라' });
-  res.send('데이터 저장 완료');
+router.get('/insert', async (req, res) => {
+  // JS Object 형태로 저장(엄밀히 JS Object는 아니고 MongoDB자료형인 BSON이다)
+  try {
+    await db.collection('post').insertOne({ 
+      title: '제발 들어가라', 
+      content: '들어갔니?' 
+    });
+    res.send('데이터 저장 완료');
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 module.exports = router;
